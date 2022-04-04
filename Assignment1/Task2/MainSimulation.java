984b409232be71cf2ef4618d78be3e9f98a5ed1a
Robin Davidsson <robin@pop-os.localdomain>
2022-04-04 16:46:48 +0200

    task2

---
+++ Assignment1/Task2/MainSimulation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+
+public class MainSimulation extends GlobalSimulation{
+ 
+    public static void main(String[] args) throws IOException {
+    	Event actEvent;
+    	State actState = new State(); // The state that shoud be used
+    	// Some events must be put in the event list at the beginning
+        insertEvent(ARRIVALA, 0);  
+        insertEvent(MEASURE, 0.1);
+        
+        // The main simulation loop
+    	while (actState.noMeasurements < 1000){
+    		actEvent = eventList.fetchEvent();
+    		time = actEvent.eventTime;
+    		actState.treatEvent(actEvent);
+    	}
+    	
+    	// Printing the result of the simulation, in this case a mean value
+    	System.out.println(1.0*actState.accumulatedA + " : " + 1.0*actState.accumulatedB);
+    }
+}
---
+++ Assignment1/Task2/State.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+class State extends GlobalSimulation{
+	
+	// Here follows the state variables and other variables that might be needed
+	// e.g. for measurements
+	public int numberInQueue = 0, accumulatedA = 0, accumulatedB = 0, noMeasurements = 0;
+
+	private int bufferA = 0, bufferB = 0, d=1;
+	private double xa = 0.002, xb=0.004, lambda=1/150d;
+
+	Random slump = new Random(); // This is just a random number generator
+	
+	
+	// The following method is called by the main program each time a new event has been fetched
+	// from the event list in the main loop. 
+	public void treatEvent(Event x){
+		switch (x.eventType){
+			case ARRIVALA:
+				arrivalA();
+				break;
+			case ARRIVALB:
+				arrivalB();
+				break;
+			case READY:
+				ready();
+				break;
+			case MEASURE:
+				measure();
+				break;
+		}
+	}
+	
+	// The following methods defines what should be done when an event takes place. This could
+	// have been placed in the case in treatEvent, but often it is simpler to write a method if 
+	// things are getting more complicated than this.
+	
+	private void arrivalA(){
+		// if there is no B signal then process A signal
+		bufferA++;
+		accumulatedA++;
+
+		if (bufferA == 0 || bufferB == 0){
+			// ready for next signal
+			insertEvent(READY, time + xa);
+		}
+		
+		// recieve next A signal
+		insertEvent(ARRIVALA, time + exp(lambda));
+	}
+
+	private void arrivalB(){
+		// add to B buffer
+		bufferB++;
+		accumulatedB++;
+		// time until ready again
+		insertEvent(READY, time + xb);
+	}
+	
+	private void ready(){
+		if (bufferB == 0)
+		{
+			bufferA--;
+			// sending back the disconnect signal
+			insertEvent(ARRIVALB, time + d);
+			// processing
+		}
+		else // else add to buffer
+		{
+			bufferB--;
+		}
+		if (bufferA > 0)
+			insertEvent(READY, time + xa);
+	}
+	
+	private void measure(){
+		//accumulated = accumulatedA;
+		noMeasurements++;
+		insertEvent(MEASURE, time + 0.1);
+	}
+
+	private double exp(double mean) {
+		return Math.log(1-slump.nextDouble())/-(1/mean);
+	}
+
+}
