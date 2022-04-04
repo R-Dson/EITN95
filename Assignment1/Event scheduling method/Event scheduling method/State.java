18ab15065bc5bb009ed6d16e029e2a400c782b38
Vanillj <robindavidsson@outlook.com>
2022-04-04 14:47:54 +0200

    added folder

--- Assignment1/Event scheduling method/Event scheduling method/State.java
+++ Assignment1/Event scheduling method/Event scheduling method/State.java
@@ -5,7 +5,7 @@ class State extends GlobalSimulation{
 	
 	// Here follows the state variables and other variables that might be needed
 	// e.g. for measurements
-	public int numberInQueue1 = 0, accumulated = 0, noMeasurements = 0, numberInQueue2 = 0, interarrivalTime = 2,numberArrived = 0, numberRejected = 0;
+	public int numberInQueue = 0, accumulated = 0, noMeasurements = 0;
 
 	Random slump = new Random(); // This is just a random number generator
 	
@@ -14,21 +14,15 @@ class State extends GlobalSimulation{
 	// from the event list in the main loop. 
 	public void treatEvent(Event x){
 		switch (x.eventType){
-		case ARRIVAL1:
-			arrival1();
-			break;
-		case ARRIVAL2:
-			arrival2();
-			break;
-		case READY1:
-			ready1();
-			break;
-		case READY2:
-			ready2();
-			break;
-		case MEASURE:
-			measure();
-			break;
+			case ARRIVAL:
+				arrival();
+				break;
+			case READY:
+				ready();
+				break;
+			case MEASURE:
+				measure();
+				break;
 		}
 	}
 	
@@ -37,45 +31,22 @@ class State extends GlobalSimulation{
 	// have been placed in the case in treatEvent, but often it is simpler to write a method if 
 	// things are getting more complicated than this.
 	
-	private void arrival1(){		
-		if(numberInQueue1<10) {
-			if (numberInQueue1 == 0)
-				insertEvent(READY1, time + exp(2.1));
-			numberInQueue1++;
-		}
-		else {
-			numberRejected++;
-		}
-		numberArrived++;
-		insertEvent(ARRIVAL1, time + interarrivalTime);	
-	}
-
-	private void ready1(){
-		numberInQueue1--;
-		if (numberInQueue1 > 0)
-			insertEvent(READY1, time + exp(2.1));
-		insertEvent(ARRIVAL2, time);
+	private void arrival(){
+		if (numberInQueue == 0)
+			insertEvent(READY, time + 2*slump.nextDouble());
+		numberInQueue++;
+		insertEvent(ARRIVAL, time + 2.5*slump.nextDouble());
 	}
 	
-	private void arrival2(){
-		if (numberInQueue2 == 0)
-			insertEvent(READY2, time + 2);
-		numberInQueue2++;
+	private void ready(){
+		numberInQueue--;
+		if (numberInQueue > 0)
+			insertEvent(READY, time + 2*slump.nextDouble());
 	}
 	
-	private void ready2(){
-		numberInQueue2--;
-		if (numberInQueue2 > 0)
-			insertEvent(READY2, time + 2);
-	}
-
 	private void measure(){
-		accumulated = accumulated + numberInQueue2;
+		accumulated = accumulated + numberInQueue;
 		noMeasurements++;
-		insertEvent(MEASURE, time + exp(5));
-	}
-	
-	private double exp(double mean) {
-		return Math.log(1-slump.nextDouble())/-(1/mean);
+		insertEvent(MEASURE, time + slump.nextDouble()*10);
 	}
 }
