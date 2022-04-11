e8c39621b8a49bba44a7546adc6e022a45ef68b5
Vanilj <robindavidsson@outlook.com>
2022-04-11 23:05:24 +0200

    task 5

---
+++ Assignment1/Task5/Gen.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+//Denna klass rver Proc, det gr att man kan anvnda time och signalnamn utan punktnotation
+//It inherits Proc so that we can use time and the signal names without dot notation 
+
+class Gen extends Proc{
+
+	//Slumptalsgeneratorn startas:
+	//The random number generator is started:
+	Random slump = new Random();
+
+	//Generatorn har tv parametrar:
+	//There are two parameters:
+	//public Proc sendTo;    //Anger till vilken process de genererade kunderna ska skickas //Where to send customers
+
+	// dispatcher time assumed to be 0
+	public ArrayList<QS> listSendTo = new ArrayList<>();
+	public double lambda;  //Hur mnga per sekund som ska generas //How many to generate per second
+	public static int totalArrived = 0, totalArrivedSpecial = 0, totalLeftSpecial = 0, totalArrivedNormal = 0, totalLeftNormal = 0;
+	//Hr nedan anger man vad som ska gras nr en signal kommer //What to do when a signal arrives
+
+	public void TreatSignal(Signal x){
+		switch (x.signalType){
+			case READY:{
+				QS reciever = (QS)getSmallestProc();
+				//System.out.println("ID: " + reciever.ID + ", NumQ: " + reciever.numberInQueue);
+				SignalList.SendSignal(ARRIVAL, reciever, time + QS.expMean(0.11));
+				//SignalList.SendSignal(READY, this, time + (2.0/lambda)*slump.nextDouble());}
+				SignalList.SendSignal(READY, this, time + QS.expLambda(lambda));}
+				break;
+		}
+	}
+
+	private Proc getRandomProc()
+	{
+		// if there is 5 available processes
+		return listSendTo.get(slump.nextInt(5));
+	}
+
+	private Proc getRoundRobinProc()
+	{
+		// start at 0
+		int selected = totalArrived % 5;
+		totalArrived++;
+		return listSendTo.get(selected);
+	}
+
+	private Proc getSmallestProc()
+	{
+		QS min = listSendTo.get(0);
+		for (int i = 1; i < listSendTo.size(); i++) {
+			if (listSendTo.get(i).numberInQueue < min.numberInQueue) {
+				min = listSendTo.get(i);
+			}
+		}
+		return min;
+	}
+}
---
+++ Assignment1/Task5/QS.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+// This class defines a simple queuing system with one server. It inherits Proc so that we can use time and the
+// signal names without dot notation
+class QS extends Proc{
+	public int numberInQueue = 0, accumulated, noMeasurements;
+	public Proc sendTo;
+	static Random slump = new Random();
+
+	public double pSpecial = 0.1, pE = 0.5;
+	public int special = 0, totalArrivedSpecial = 0, totalLeftSpecial = 0, 
+				normal = 0, totalArrivedNormal = 0, totalLeftNormal = 0, ID = 0;
+
+	public void TreatSignal(Signal x){
+		switch (x.signalType){
+			case ARRIVAL:{
+				int person = whoArrives();
+				switch (person) {
+					case 0:
+						special++;
+						totalArrivedSpecial++;
+						Gen.totalArrivedSpecial++;
+						break;
+					/*case 1:
+						special++;
+						break;*/
+					case 1:
+						normal++;
+						totalArrivedNormal++;
+						Gen.totalArrivedNormal++;
+						break;
+				}
+
+				numberInQueue++;
+				if (numberInQueue == 1){
+					SignalList.SendSignal(READY,this, time + expMean(0.5));
+				}
+			} break;
+
+			case READY:{
+				numberInQueue--;
+				if (special > 0) {
+					special--;
+					totalLeftSpecial++;
+					Gen.totalLeftSpecial++;
+				}
+				else if (normal > 0) {
+					normal--;
+					totalLeftNormal++;
+					Gen.totalLeftNormal++;
+				}
+
+				/*if (sendTo != null){
+					SignalList.SendSignal(ARRIVAL, sendTo, time + exp(5));
+				}*/
+
+				if (numberInQueue > 0){
+					SignalList.SendSignal(READY, this, time + expMean(0.5));
+				}
+			} break;
+
+			case MEASURE:{
+				noMeasurements++;
+				accumulated = accumulated + numberInQueue;
+				SignalList.SendSignal(MEASURE, this, time + expMean(0.5));
+			} break;
+		}
+	}
+
+	private int whoArrives()
+	{
+		int length = 1000;
+		int ran = slump.nextInt(length);
+		double prob = pSpecial*length;
+		if (ran < prob) 
+			return 0;
+		//else if (ran <= (pA + pB)*length)
+			//return 1;
+		else
+			return 1;
+	}
+
+	// mean = 1/lambda
+	public static double expMean(double mean) {
+		return Math.log(1-slump.nextDouble())/(-1/mean);
+	}
+
+	public static double expLambda(double lambda) {
+		return expMean(1/lambda);
+	}
+}
