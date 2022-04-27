1bf0e67f85a3da8d0626872464670b6ca282f646
Vanillj <robindavidsson@outlook.com>
2022-04-27 17:31:52 +0200

    cleaned

--- Assignment1/Task5/Gen.java
+++ Assignment1/Task5/Gen.java
@@ -1,33 +1,24 @@
 import java.util.*;
 import java.io.*;
 
-//Denna klass rver Proc, det gr att man kan anvnda time och signalnamn utan punktnotation
-//It inherits Proc so that we can use time and the signal names without dot notation 
-
 class Gen extends Proc {
 
-	//Slumptalsgeneratorn startas:
-	//The random number generator is started:
 	Random slump = new Random();
 
-	//Generatorn har tv parametrar:
-	//There are two parameters:
-	//public Proc sendTo;    //Anger till vilken process de genererade kunderna ska skickas //Where to send customers
 
 	// dispatcher time assumed to be 0
 	public ArrayList<QS> listSendTo = new ArrayList<>();
-	public double mean;  //Hur mnga per sekund som ska generas //How many to generate per second
+	public double mean;  
 	public static int totalArrived = 0, totalQueueMeasure = 0, totalNumMeasures = 0;
 	public int totalInQueue = 0;
 	public int numberMeasurements = 0;
-	//Hr nedan anger man vad som ska gras nr en signal kommer //What to do when a signal arrives
 
 	public void TreatSignal(Signal x){
 		switch (x.signalType){
 			case READY:{
 				QS reciever = (QS)getRandomProc();
 				SignalList.SendSignal(ARRIVAL, reciever, time);
-				double tmp = slump.nextDouble()*0.11*2;
+				double tmp = slump.nextDouble()*mean*2;
 				SignalList.SendSignal(READY, this, time+tmp);}
 				break;
 			case MEASURE:{
