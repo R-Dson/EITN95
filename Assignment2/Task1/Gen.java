0e848f819cd2d7ca2bdb9f419a5b9e6622ddd6a9
Vanillj <robindavidsson@outlook.com>
2022-05-02 13:37:18 +0200

    CI

--- Assignment2/Task1/Gen.java
+++ Assignment2/Task1/Gen.java
@@ -8,25 +8,32 @@ class Gen extends Proc{
 	public ArrayList<QS> listSendTo = new ArrayList<>();
 	public double lambda; 
 	public static int totalArrived = 0, totalNumMeasures = 0;
+	public ArrayList<Integer> inQueue = new ArrayList<>();
+	public double deltaM = 0;
 	
 	public void TreatSignal(Signal x){
 		switch (x.signalType){
 			case READY:{
 				QS reciever = getFirstAvailable();
 				if (reciever != null) {
-					System.out.println("ID: " + reciever.ID + ", NumArrived: " + totalArrived + ", numMeasures: " + totalNumMeasures);
+					//System.out.println("ID: " + reciever.ID + ", NumArrived: " + totalArrived + ", numMeasures: " + totalNumMeasures);
 					SignalList.SendSignal(ARRIVAL, reciever, time);
 				}
 				// next arrival
 				SignalList.SendSignal(READY, this, time + QS.expLambda(lambda));
 			} break;
 			case MEASURE:{
+				int i = 0;
 				for (QS q : listSendTo) {
 					if (q.occupied)
+					{
+						i++;
 						totalArrived += 1;
+					}
 				}
+				inQueue.add(i);
 				totalNumMeasures++;
-				SignalList.SendSignal(MEASURE, this, time+1);
+				SignalList.SendSignal(MEASURE, this, time+deltaM);
 				break;
 			}
 		}
--- Assignment2/Task1/MainSimulation.java
+++ Assignment2/Task1/MainSimulation.java
@@ -9,9 +9,11 @@ public class MainSimulation extends Global{
     	new SignalList();
 
     	Gen Generator = new Gen();
-    	Generator.lambda = 8; 
-		int Nservers = 1000;
-		int ServiceTimeX = 100;
+    	Generator.lambda = 4; 
+		Generator.deltaM = 4;
+		int Nservers = 100;
+		int ServiceTimeX = 10;
+		int numMeasures = 1000;
 
 		for (int i = 0; i < Nservers; i++) {
 			QS Qtemp = new QS();
@@ -23,7 +25,7 @@ public class MainSimulation extends Global{
     	SignalList.SendSignal(READY, Generator, 0);
 		SignalList.SendSignal(MEASURE, Generator, 1);
 
-    	while (Gen.totalNumMeasures < 1000){
+    	while (Gen.totalNumMeasures < numMeasures){
     		actSignal = SignalList.FetchSignal();
     		time = actSignal.arrivalTime;
     		actSignal.destination.TreatSignal(actSignal);
@@ -32,8 +34,33 @@ public class MainSimulation extends Global{
     	System.out.println("Time:" + time);
 		System.out.println("------------------------------------------------------");
     	System.out.println("Mean arrived: " + 1.0*Gen.totalArrived/Gen.totalNumMeasures);
-		System.out.println("totalArrived: " + 1.0*Gen.totalArrived);
-		System.out.println("totalNumMeasures: " + 1.0*Gen.totalNumMeasures);
+
+		double mMean = 0;
+		for (Integer i : Generator.inQueue) {
+			mMean += i;
+		}
+		mMean /= Generator.inQueue.size();
+
+		double std = calcStandardDevEstimate(mMean, Generator.inQueue);
+
+        System.out.println();
+        System.out.println("Std estimation: \t" + String.format("%.4f", std));
+        System.out.println("CI length: \t" + String.format("%.4f", 2 * 1.96 * std));
+        System.out.println("95% confidence interval:");
+        System.out.println("\t" + String.format("%.4f", mMean) + " ± " + String.format("%.4f", 1.96 * std));
 
     }
+
+	private static double calcStandardDevEstimate(double mMean, List<Integer> means) {
+        double sum = 0;
+        int M = means.size();
+        for (int i = 0; i < M; i++) {
+            sum += Math.pow(means.get(i) - mMean, 2);
+        }
+
+        // NOTE! Using Bessel's correction, see wiki
+        return Math.sqrt(sum / (double) (M - 1));
+    }
+
+
 }
