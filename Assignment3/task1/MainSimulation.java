8637958ecfb14dfc58bc365c98d9d704c8c7bee7
oliver_persson_b <oliver.persson.b@gmail.com>
2022-05-19 10:29:22 +0200

    changed so main sim can run several iterations for 1 config

--- Assignment3/task1/MainSimulation.java
+++ Assignment3/task1/MainSimulation.java
@@ -1,14 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainSimulation extends Global {
 
   public static void main(String[] args) throws IOException {
-    runSim();
+    runAll(15);
   }
 
-  public static void runSim() throws IOException {
+  private static void runAll(int iter) {
+    ArrayList<Double> mmTput = new ArrayList<>();
+    ArrayList<Double> stdsTput = new ArrayList<>();
+    ArrayList<Double> mmPLoss = new ArrayList<>();
+    ArrayList<Double> stdsPLoss = new ArrayList<>();
+
+    ArrayList<Double> meansT;
+    ArrayList<Double> meansP;
+    for (int i = 0; i < iter; i++) {
+      System.out.println("----------------");
+      System.out.println("iteration = " + i);
+
+      ArrayList<ArrayList<Double>> res = runOneSim();
+      meansT = res.get(0);
+      meansP = res.get(1);
+
+      double mMean = Statistics.calcMean(meansT);
+      mmTput.add(mMean);
+      stdsTput.add(Statistics.calcStandardDevEstimate(mMean, meansT));
+
+      mMean = Statistics.calcMean(meansP);
+      mmPLoss.add(mMean);
+      stdsPLoss.add(Statistics.calcStandardDevEstimate(mMean, meansP));
+
+      System.out.println("----------------");
+    }
+
+    writeResultsToFile(mmTput, "a3p4_Tput_mMean_vary_r.txt");
+    writeResultsToFile(stdsTput, "a3p4_Tput_stds_vary_r.txt");
+    writeResultsToFile(mmPLoss, "a3p4_PLoss_mMean_vary_r.txt");
+    writeResultsToFile(stdsPLoss, "a3p4_PLoss_stds_vary_r.txt");
+  }
+
+  public static ArrayList<ArrayList<Double>> runOneSim() throws IOException {
     new SignalList();
     Signal actSignal;
 
@@ -42,119 +78,27 @@ public class MainSimulation extends Global {
 
     double mean = Statistics.calcMean(gateway.Tputs);
 
-    System.out.println("nbr of transmissions: " + gateway.nbrOfTransmissions);
-    System.out.println("nbr of success: " + gateway.nbrOfSuccess);
-    System.out.println(mean);
+    // System.out.println("nbr of transmissions: " + gateway.nbrOfTransmissions);
+    // System.out.println("nbr of success: " + gateway.nbrOfSuccess);
+    // System.out.println(mean);
+    ArrayList<ArrayList<Double>> res = new ArrayList<>();
+
+    res.add(gateway.Tputs);
+    res.add(gateway.PLosses);
+
+    return res;
   }
 
-  static Config getConfig() {
-    int n = 1000;
-    int ts = 4000;
-    double r = 7000;
-    double Tp = 1;
-    double timeBetweenSamples = 2000;
-    int nbrOfMeasurements = 100;
-
-    Random slump = new Random();
-    ArrayList<Integer> xlist = new ArrayList<>();
-    ArrayList<Integer> ylist = new ArrayList<>();
-
-    for (int i = 0; i < n; i++) {
-      int xSlump = slump.nextInt(10000);
-      int ySlump = slump.nextInt(10000);
-
-      // Can't overlap
-      while (xlist.contains(xSlump) && ylist.contains(ySlump)) {
-        xSlump = slump.nextInt(10000);
-        ySlump = slump.nextInt(10000);
+  static <T> void writeResultsToFile(Iterable<T> ys, String fileName) {
+    File file = new File(fileName);
+    try (FileWriter fw = new FileWriter(file, StandardCharsets.UTF_8)) {
+      for (T y : ys) {
+        fw.write(y + "\n");
       }
 
-      xlist.add(xSlump);
-      ylist.add(ySlump);
-
+      fw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    Integer[] xs = xlist.toArray(new Integer[n]);
-    Integer[] ys = ylist.toArray(new Integer[n]);
-
-    return new Config(n, ts, Tp, r, timeBetweenSamples, nbrOfMeasurements, xs, ys);
-  }
-
-  // -------------------------------------------------------
-  // -------------------------------------------------------
-  // -------------------------------------------------------
-
-  static void testGatewayCollisions() {
-    Config config = getConfig();
-    Gateway gateway = new Gateway(config.r, config.timeBetweenSamples, config.Tp);
-    Coord c1 = new Coord(1, 1);
-    Coord c2 = new Coord(1, 2);
-    System.out.println("Distance: " + c1.distance(c2));
-    System.out.println("Should be true due to overlap: " + (c1.distance(c2) <= config.r));
-    c1 = new Coord(1, 1);
-    c2 = new Coord(1, 10000);
-    System.out.println("Distance: " + c1.distance(c2));
-    System.out.println("Should be false due: " + (c1.distance(c2) <= config.r));
-  }
-
-  public static void runDebugSim() throws IOException {
-    new SignalList();
-    Signal actSignal;
-
-    Config config = getDebugConfig();
-
-    Gateway gateway = new Gateway(config.r, config.timeBetweenSamples, config.Tp);
-    SignalList.SendSignal(MEASURE, gateway, time + config.timeBetweenSamples);
-
-    // start sensors cycles
-    for (int i = 0; i < config.n; i++) {
-      int x = config.xs[i];
-      int y = config.ys[i];
-      Coord coord = new Coord(x, y);
-      Sensor sensor = new Sensor(coord, gateway, config.ts, config.isSmart, config.lb, config.ub);
-      // stochastic, otherwise a lot of collisions immediately
-      SignalList.SendSignal(SEND_MESSAGE, sensor, time);
-    }
-
-    int iter = 0;
-    while (iter < config.nbrOfMeasurements) {
-      actSignal = SignalList.FetchSignal();
-      if (actSignal.signalType == MEASURE) {
-        iter++;
-      }
-      time = actSignal.arrivalTime;
-      actSignal.destination.TreatSignal(actSignal);
-    }
-
-    double meanSum = gateway.Tputs.stream()
-        .reduce(0.0, (subtotal, element) -> subtotal + element);
-    double mean = (1.0 / config.nbrOfMeasurements) * meanSum;
-
-    System.out.println("nbr of transmissions: " + gateway.nbrOfTransmissions);
-    System.out.println("nbr of success: " + gateway.nbrOfSuccess);
-    System.out.println(mean);
-  }
-
-  static Config getDebugConfig() {
-    int n = 3;
-    int ts = 1000;
-    double r = 7000;
-    double Tp = 10000;
-    double timeBetweenSamples = 1000;
-    int nbrOfMeasurements = 1;
-
-    ArrayList<Integer> xlist = new ArrayList<>();
-    ArrayList<Integer> ylist = new ArrayList<>();
-    xlist.add(1);
-    xlist.add(1);
-    xlist.add(1);
-    ylist.add(1);
-    ylist.add(2);
-    ylist.add(3);
-
-    Integer[] xs = xlist.toArray(new Integer[n]);
-    Integer[] ys = ylist.toArray(new Integer[n]);
-
-    return new Config(n, ts, Tp, r, timeBetweenSamples, nbrOfMeasurements, xs, ys);
   }
 }
